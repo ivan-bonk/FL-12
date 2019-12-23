e8e8fd326245308fbb630d69e4aec01ea9bf24a9
Ivan <vaaeking@gmail.com>
2019-12-23 21:40:53 +0200

    done hw6

--- FL12_HW6/homework/js/task1.js
+++ FL12_HW6/homework/js/task1.js
@@ -1,1 +1,25 @@
-// Your code goes here
+let a = parseFloat(prompt('Enter A','')),
+    b = parseFloat(prompt('Enter B','')),
+    c = parseFloat(prompt('Enter C','')),
+    x1 = 0,
+    x2 = 0; 
+const FOUR = 4,
+      TWO = 2;
+
+if(isNaN(a) || isNaN(b) || isNaN(c)) {
+    alert('Invalid input data');
+} else {
+    let d = b*b - FOUR*a*c;
+    if(d < 0) {
+        alert('No solution ');
+    } else {
+        x1 = (-b + Math.sqrt(d)) / (TWO * a); 
+        x2 = (-b - Math.sqrt(d)) / (TWO * a);
+
+        if(d > 0) {
+            alert('x1 = ' + x1 + ' and x2 = ' + x2);
+        } else if(d === 0) {
+            alert('x = ' + x1);
+        }
+    }
+}
--- FL12_HW6/homework/js/task2.js
+++ FL12_HW6/homework/js/task2.js
@@ -1,1 +1,23 @@
-// Your code goes here
+let a = parseFloat(prompt('Enter A','')),
+    b = parseFloat(prompt('Enter B','')),
+    c = parseFloat(prompt('Enter C',''));
+
+if(isNaN(a) || isNaN(b) || isNaN(c)) {
+    alert('input values should be ONLY numbers');
+    console.log('input values should be ONLY numbers');
+} else if(a === 0 || b === 0 || c ===0) {
+        alert('A triangle must have 3 sides with a positive definite length');
+        console.log('A triangle must have 3 sides with a positive definite length');
+    } else if(c >= a + b || b >= a + c || a >= b + c) {
+        alert('Triangle doesn’t exist');
+        console.log('Triangle doesn’t exist');
+    } else if(a === b && a === c) {
+        alert('Equilateral triangle');
+        console.log('Equilateral triangle');
+    } else if(a === b || a === c || b === c) {
+        alert('Isosceles triangle');
+        console.log('Isosceles triangle');
+    } else {
+        alert('Scalene triangle');
+        console.log('Scalene triangle');
+    }
